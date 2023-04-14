689ff79273fcd68195667b621b069b7a7f336c91
Belle379 <isabllelsn@gmail.com>
2023-04-14 10:37:13 -0300

    t

--- users/cuidador/cadastrarCuidador.php
+++ users/cuidador/cadastrarCuidador.php
@@ -49,6 +49,26 @@ if ($result_servicoC > 0) {
     echo "Deu erro:  <br>" . mysqli_error($conexao);
 }
 
+//.................................................Tabela Endereço.....................................................//
+
+$cep = $_POST["cep"];
+$uf = $_POST["uf"];
+$cidade = $_POST["cidade"];
+$bairro = $_POST["bairro"];
+$endereco = $_POST["endereco"];
+$numero = $_POST["numero"];
+
+
+$query_endereco = "INSERT INTO tb_endereco (cep,uf,cidade,bairro,endereco,numero) 
+        VALUES ('$cep', '$uf','$cidade','$bairro','$endereco' ,'$numero')";
+$result_endereco = mysqli_query($conexao, $query_endereco);
+        
+if ($result_endereco > 0) {
+        echo "<script>alert('Seus dados foram salvos !')</script>";
+    } else {
+        echo "Deu erro:  <br>" . mysqli_error($conexao);
+    }    
+
 
 mysqli_close($conexao);
 ?>
