16b0e3275e4e554aae5ab01bf712a3c26349883f
Belle379 <isabllelsn@gmail.com>
2023-05-02 10:09:55 -0300

    .

--- log/logmo.php
+++ log/logmo.php
@@ -1,43 +1,13 @@
 <?php
-session_start();
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-include "conexao.php";
-
-$email = mysqli_real_escape_string($conexao, trim($_POST['email_user']));
-$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha_user'])));
-
-       /*print_r('Email: ' . $email);
-       print_r('<br>');
-       print_r('Senha: ' . $senha);
-	   die;*/
-
-$codlogin = "SELECT * FROM tb_user WHERE email_user =  '$email'";
-
-$result = mysqli_query($conexao, $codlogin);
-$row = mysqli_num_rows($result);
-
-if($email == ['moderador@carrapicho.com'] && $senha == ['carra123']){
-	
-	$bd_arr = mysqli_fetch_array($result);
-	
-	mysqli_close($conexao);
-	
-	if($bd_arr['senha_user'] == $senha){	
-		header("Location: /./carrapichoSenac/feed.php");
-		
-		
-		$_SESSION['nome_user'] = $bd_arr['nome_user'];
-		$_SESSION['logado'] = true;
-		$_SESSION['id_user'] = $bd_arr['id_user']; 
-	
-		//var_dump($row);
-		//die;		
-	
-	}else{
-		echo "Por favor verificar senha !";
-	}
-
-}else{
-	echo "Não foi encontrado nenhum cadastro para esse E-mail!";
+if ($email === 'moderador@carrapicho.com' && $password === 'carra123') {
+  // Se as credenciais estiverem corretas, redirecione o usuário para a página de boas-vindas
+  header("Location: welcome.php");
+  exit();
+} else {
+  // Se as credenciais estiverem incorretas, mostre uma mensagem de erro
+  echo "E-mail ou senha incorretos. Tente novamente.";
 }
 ?>
